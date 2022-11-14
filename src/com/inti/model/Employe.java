975aa042e4fe3d0a7ec6130254d0529fff6f95ab
OussamaINTI <ayari_oussama26@hotmail.fr>
2022-11-14 15:09:00 +0100

    ajout deux variables dans le fichier Employe.java

--- src/com/inti/model/Employe.java
+++ src/com/inti/model/Employe.java
@@ -4,11 +4,15 @@ public class Employe {
 	private Long idEmploye;
 	private String nomEmploye;
 	private int age;
-	private float salaireeeeeeeeeeeeeee;
+	private float salaire;
+	private String username;
+	private String password;
+
 	public Employe() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
+
 	public Employe(Long idEmploye, String nomEmploye, int age, float salaire) {
 		super();
 		this.idEmploye = idEmploye;
@@ -16,34 +20,59 @@ public class Employe {
 		this.age = age;
 		this.salaire = salaire;
 	}
+
 	public Long getIdEmploye() {
 		return idEmploye;
 	}
+
 	public void setIdEmploye(Long idEmploye) {
 		this.idEmploye = idEmploye;
 	}
+
 	public String getNomEmploye() {
 		return nomEmploye;
 	}
+
 	public void setNomEmploye(String nomEmploye) {
 		this.nomEmploye = nomEmploye;
 	}
+
 	public int getAge() {
 		return age;
 	}
+
 	public void setAge(int age) {
 		this.age = age;
 	}
+
 	public float getSalaire() {
 		return salaire;
 	}
+
 	public void setSalaire(float salaire) {
 		this.salaire = salaire;
 	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 	@Override
 	public String toString() {
 		return "Employe [idEmploye=" + idEmploye + ", nomEmploye=" + nomEmploye + ", age=" + age + ", salaire="
 				+ salaire + "]";
 	}
-	
+
 }
