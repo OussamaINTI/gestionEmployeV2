7d5513e8076f19f1deb28d0b77083b0099955cd9
OussamaINTI <ayari_oussama26@hotmail.fr>
2022-11-14 15:48:08 +0100

    ne msg

--- src/com/inti/test/Test.java
+++ src/com/inti/test/Test.java
@@ -5,6 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Bonjour à tous");
 		System.out.println("Bonjour Robin");
+		System.out.println("Bonjour Oussama");
 
 	}
 
