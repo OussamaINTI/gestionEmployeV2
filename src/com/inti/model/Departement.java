3a5003c5b3b46d4d501ca2d3c836de71f1fda5c9
OussamaINTI <ayari_oussama26@hotmail.fr>
2022-11-15 09:45:26 +0100

    changement dans la class dept

--- src/com/inti/model/Departement.java
+++ src/com/inti/model/Departement.java
@@ -1,5 +1,26 @@
 package com.inti.model;
 
 public class Departement {
+	private Long idDepartement;
 
+	public Departement() {
+	}
+
+	public Departement(Long idDepartement) {
+		this.idDepartement = idDepartement;
+	}
+
+	public Long getIdDepartement() {
+		return idDepartement;
+	}
+
+	public void setIdDepartement(Long idDepartement) {
+		this.idDepartement = idDepartement;
+	}
+
+	@Override
+	public String toString() {
+		return "Departement [idDepartement=" + idDepartement + "]";
+	}
+	
 }
