62361775ce1a75324095e0564534c23e89ced782
aspalrobin <robin.colliaux@gmail.com>
2022-11-15 09:41:17 +0100

    impl calsse departement

--- src/com/inti/model/Departement.java
+++ src/com/inti/model/Departement.java
@@ -1,5 +1,26 @@
 package com.inti.model;
 
 public class Departement {
+	private Long idDept;
 
+	public Departement() {
+	}
+
+	public Departement(Long idDept) {
+		this.idDept = idDept;
+	}
+
+	public Long getIdDept() {
+		return idDept;
+	}
+
+	public void setIdDept(Long idDept) {
+		this.idDept = idDept;
+	}
+
+	@Override
+	public String toString() {
+		return "Departement [idDept=" + idDept + "]";
+	}
+	
 }
